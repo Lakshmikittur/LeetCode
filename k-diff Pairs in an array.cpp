c9f273d4925f7aa4d6eb27c898a70610094a37cd
Lakshmi Kittur <kittur.lakshmi@gmail.com>
2020-10-05 17:05:28 +0530

    Update and rename k-diff Pairs in an array to k-diff Pairs in an array.cpp

--- k-diff Pairs in an array.cpp
+++ k-diff Pairs in an array.cpp
@@ -1,3 +1,12 @@
+/* Question:Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+
+A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+
+0 <= i, j < nums.length
+i != j
+a <= b
+b - a == k */
+
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
